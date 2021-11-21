6e0fb0b157d8597e89242f0c58e3db4594664875
jj1guj <jj1guj@jj1guj.net>
2021-11-21 11:34:49 +0900

    divided file

--- src/board.cpp
+++ src/board.cpp
@@ -1,3 +1,4 @@
+#include "board.hpp"
 unsigned long long makeLegalBoard(unsigned long long board_player,unsigned long long board_opponent){
     //左右端の番人
     unsigned long long horizontalWatcher=board_opponent&0x7e7e7e7e7e7e7e7e;
@@ -110,4 +111,98 @@ int bitcount(unsigned long long data){
     data=(data&0xffff0000ffff)+((data&0xffff0000ffff0000)>>16);//32桁ごとの1の数
     data=(data&0xffffffff)+((data&0xffffffff00000000)>>32);//64桁の1の数
     return data;
+}
+
+//指定した座標に何色の石がおいてあるか返す
+int Board::operator [](int i){
+    i=63-i;
+    if((board_black>>i)&1){
+        return 1;
+    }else if((board_white>>i)&1){
+        return -1;
+    }else{
+        return 0;
+    }
+}
+
+//座標をビットに変換
+unsigned long long Board::idToBit(int id){
+    unsigned long long mask=0x8000000000000000;
+    unsigned long long x=id>>3,y=id&7;
+    mask=mask>>y;
+    mask=mask>>(x*8);
+    return mask;
+}
+
+//着手できるか判定
+bool Board::putable(unsigned long long id){
+    unsigned long long legalBoard=makeLegalBoard(board_player,board_opponent);
+
+    return (id&legalBoard)==id;
+}
+
+//着手
+void Board::push(int id_int){
+    if(0<=id_int&&id_int<64){
+        unsigned long long id=idToBit(id_int);
+        unsigned long long rev=0;
+        
+        for(int i=0;i<8;++i){
+            unsigned long long rev_=0;
+            unsigned long long mask=transfer(id,i);
+            
+            while((mask!=0)&&((mask&board_opponent)!=0)){
+                rev_|=mask;
+                mask=transfer(mask,i);
+            }
+
+            if((mask&board_player)!=0){
+                rev|=rev_;
+            }
+        }
+
+        //反転する
+        board_player^=(id|rev);
+        board_opponent^=rev;
+    }
+
+    //手番等の更新
+    swapBoard();
+    ++ply;
+}
+
+//パスの判定
+bool Board::is_pass(){
+    unsigned long long playerLegalBoard=makeLegalBoard(board_player,board_opponent);
+    unsigned long long opponentLegalBoard=makeLegalBoard(board_opponent,board_player);
+
+    return (playerLegalBoard == 0x0000000000000000) && (opponentLegalBoard != 0x0000000000000000);
+}
+
+//終局の判定
+bool Board::is_over(){
+    unsigned long long playerLegalBoard=makeLegalBoard(board_player,board_opponent);
+    unsigned long long opponentLegalBoard=makeLegalBoard(board_opponent,board_player);
+
+    return (playerLegalBoard == 0x0000000000000000) && (opponentLegalBoard == 0x0000000000000000);
+}
+
+//手番交代
+void Board::swapBoard(){
+    unsigned long long tmp=board_player;
+    board_player=board_opponent;
+    board_opponent=tmp;
+    turn=!turn;
+
+    if(turn){
+        board_white=board_player;
+        board_black=board_opponent;
+    }else{
+        board_black=board_player;
+        board_white=board_opponent;
+    }
+
+    //枚数の更新
+    point[0]=bitcount(board_black);
+    point[1]=bitcount(board_white);
 }
