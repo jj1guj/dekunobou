65d13160aab69425b3a7333131c851cc6f763e46
jj1guj <jj1guj@jj1guj.net>
2021-11-21 12:59:54 +0900

    enable to play with human

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include"play.hpp"
+#include<fstream>
+#include<istream>
+#include<vector>
+#include<omp.h>
+#include<sstream>
+
+float param[param_size];
+
+//対局用の評価関数の読み込み
+int load_eval(std::string filename,float param[param_size]){
+    std::ifstream inputs(filename);
+    std::string s;
+    int i=0;
+    if(inputs.fail()){
+        std::cout<<"Failed to openfile\n";
+        return -1;
+    }
+    while(getline(inputs,s)){
+        param[i]=std::stof(s);
+        std::cout<<param[i]<<std::endl;
+        ++i;
+    }
+    return 0;
+}
+
+int main(int argc,char** argv){
+    int threads=0;
+    if(argc>1)threads=atoi(argv[1]);
+
+    load_eval("eval.txt",param);
+    //人との対決用
+    // play_engine_human(param);
+    play_with_human();
+}
