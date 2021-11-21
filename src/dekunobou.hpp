96fab8d47ef638510f9c863debe1bed678fcd0bd
jj1guj <jj1guj@jj1guj.net>
2021-11-21 20:05:54 +0900

    added extra includes

--- src/dekunobou.hpp
+++ src/dekunobou.hpp
@@ -1,14 +1,19 @@
 #include<iostream>
 #include<algorithm>
 #include<random>
+#include<string>
 #include<vector>
+#include<fstream>
+#include<istream>
+#include<omp.h>
+#include<sstream>
 
 #pragma once
 //モード指定
 #define Debug //デバッグモード
 #define Console //コンソールモードでの対局(default)
-//#define GA //遺伝的アルゴリズムによる評価関数生成
-#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
+#define GA //遺伝的アルゴリズムによる評価関数生成
+//#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
 
 #define perfect_search 11 //完全読みを始めるタイミング
 #define param_size 489
