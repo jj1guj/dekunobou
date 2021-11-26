6df36cb68f92fcb540ba536be812bdf44158f99d
jj1guj <jj1guj@jj1guj.net>
2021-11-26 20:42:58 +0900

    changed evaluate function

--- src/dekunobou.hpp
+++ src/dekunobou.hpp
@@ -18,13 +18,13 @@
 //#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
 
 #define perfect_search 11 //完全読みを始めるタイミング
-// #define param_size 489
-#define param_size 165
+#define param_size 489
+// #define param_size 165
 #define inf 1000000000
 
-// #define cur_opening 0
-// #define cur_middle 163
-// #define cur_ending 326
-#define cur_opening 162
+#define cur_opening 0
 #define cur_middle 163
-#define cur_ending 164
+#define cur_ending 326
+// #define cur_opening 162
+// #define cur_middle 163
+// #define cur_ending 164
