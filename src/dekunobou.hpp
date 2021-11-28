5a839e8e4e1e89c4e36578bf61bf1e0689e3884d
jj1guj <jj1guj@jj1guj.net>
2021-11-28 21:23:11 +0900

    set api mode

--- src/dekunobou.hpp
+++ src/dekunobou.hpp
@@ -14,8 +14,8 @@
 //モード指定
 //#define Debug //デバッグモード
 //#define Console //コンソールモードでの対局(default)
-#define GA //遺伝的アルゴリズムによる評価関数生成
-//#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
+//#define GA //遺伝的アルゴリズムによる評価関数生成
+#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
 
 #define perfect_search 11 //完全読みを始めるタイミング
 #define param_size 327
