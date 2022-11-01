894eaf6638bcc778769d23573d0a4e73825a70da
jj1guj <jj1guj@jj1guj.net>
2022-11-02 02:35:38 +0900

    minor changes

--- src/dekunobou.hpp
+++ src/dekunobou.hpp
@@ -13,10 +13,10 @@
 
 #pragma once
 //モード指定
-//#define Debug //デバッグモード
-//#define Console //コンソールモードでの対局(default)
-#define GA //遺伝的アルゴリズムによる評価関数生成
-//#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
+// #define Debug //デバッグモード
+// #define Console //コンソールモードでの対局(default)
+// #define GA //遺伝的アルゴリズムによる評価関数生成
+#define API //https://jj1guj.net/dekunobou-web/ 向けのAPI用バイナリ
 
 #define perfect_search 13 //完全読みを始めるタイミング
 // #define param_size 489
