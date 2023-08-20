d1450910a66275f1f8a28d20ea17c95b78fc91f8
jj1guj <jj1guj@jj1guj.net>
2023-08-20 23:43:19 +0900

    apply formatter

--- utils/battle/battle.py
+++ utils/battle/battle.py
@@ -1,4 +1,4 @@
-#指定した回数だけ対戦し, 対戦結果等を表示する
+# 指定した回数だけ対戦し, 対戦結果等を表示する
 import random
 import subprocess
 import tqdm
@@ -8,77 +8,81 @@ from board import *
 from legalmovelist import *
 
 with open("config.yaml") as f:
-    config=yaml.safe_load(f)
+    config = yaml.safe_load(f)
 
-def go(board,engine_path):
-    #エンジンに実行権限を与える
-    subprocess.run(["chmod","u+x",engine_path])
-    cp=subprocess.run([engine_path,str(board),str(int(board.turn))],encoding="utf-8",stdout=subprocess.PIPE)
+
+def go(board, engine_path):
+    # エンジンに実行権限を与える
+    subprocess.run(["chmod", "u+x", engine_path])
+    cp = subprocess.run([engine_path, str(board), str(
+        int(board.turn))], encoding="utf-8", stdout=subprocess.PIPE)
     return int(cp.stdout)
 
-def match(engine_black,engine_white,kif=None):
+
+def match(engine_black, engine_white, kif=None):
     if kif is not None:
-        board=Board(kif)
+        board = Board(kif)
     else:
-        board=Board()
+        board = Board()
 
-    pass_count=0
+    pass_count = 0
     while True:
-        if pass_count>1:
+        if pass_count > 1:
             break
-        moves=LegalMoveList(board)
+        moves = LegalMoveList(board)
 
-        if len(moves)==0:
+        if len(moves) == 0:
             board.push(64)
-            pass_count+=1
+            pass_count += 1
             continue
-        
-        pass_count=0
 
-        #後手番
+        pass_count = 0
+
+        # 後手番
         if board.turn:
-            move=go(board,engine_white)
+            move = go(board, engine_white)
         else:
-            move=go(board,engine_black)
-        
-        #着手
+            move = go(board, engine_black)
+
+        # 着手
         board.push(move)
-    
-    if board.point[0]>board.point[1]:
+
+    if board.point[0] > board.point[1]:
         return 0
-    elif board.point[0]<board.point[1]:
+    elif board.point[0] < board.point[1]:
         return 1
     else:
         return 2
 
-if __name__=="__main__":
-    result=[0,0,0] #[engine1の勝ち数,engine2の勝ち数,引き分け回数]
 
-    L=None
+if __name__ == "__main__":
+    result = [0, 0, 0]  # [engine1の勝ち数,engine2の勝ち数,引き分け回数]
+
+    L = None
     if config["book_path"] is not None:
         with open(config["book_path"]) as f:
-            L=f.readlines()
-            L=[i.replace("\n","") for i in L]
-            L=[i for i in L if len(i)//2<=20] #20手以下の進行に絞る
-            #対局回数分だけサンプリングする
-            #先後入れ替えて対局するため対局回数の半分になっている
-            L=random.sample(L,config["match_times"]//2)
+            L = f.readlines()
+            L = [i.replace("\n", "") for i in L]
+            L = [i for i in L if len(i)//2 <= 20]  # 20手以下の進行に絞る
+            # 対局回数分だけサンプリングする
+            # 先後入れ替えて対局するため対局回数の半分になっている
+            L = random.sample(L, config["match_times"]//2)
 
-    print("engine1:",config["engine1"])
-    print("engine2:",config["engine2"])
+    print("engine1:", config["engine1"])
+    print("engine2:", config["engine2"])
     for i in tqdm.tqdm(range(config["match_times"]//2)):
         if L is not None:
-            r1=match(config["engine1"],config["engine2"],L[i])
-            r2=match(config["engine2"],config["engine1"],L[i])
+            r1 = match(config["engine1"], config["engine2"], L[i])
+            r2 = match(config["engine2"], config["engine1"], L[i])
         else:
-            r1=match(config["engine1"],config["engine2"])
-            r2=match(config["engine2"],config["engine1"])
+            r1 = match(config["engine1"], config["engine2"])
+            r2 = match(config["engine2"], config["engine1"])
 
-        result[r1]+=1
-        if r2==2:
-            result[r2]+=1
+        result[r1] += 1
+        if r2 == 2:
+            result[r2] += 1
         else:
-            result[(r2+1)%2]+=1
-    print("{}: {} win".format(config["engine1"],result[0]))
-    print("{}: {} win".format(config["engine2"],result[1]))
+            result[(r2+1) % 2] += 1
+    print("{}: {} win".format(config["engine1"], result[0]))
+    print("{}: {} win".format(config["engine2"], result[1]))
     print("draw: {}".format(result[2]))
--- utils/visualizer/param_visualize.py
+++ utils/visualizer/param_visualize.py
@@ -1,74 +1,76 @@
-#これを参考にした
-#https://spcx8.hatenablog.com/entry/2018/10/09/070000
-#https://tomosoft.jp/design/?p=29005
+# これを参考にした
+# https://spcx8.hatenablog.com/entry/2018/10/09/070000
+# https://tomosoft.jp/design/?p=29005
+import yaml
+import tqdm
+import struct
+import os
+import numpy as np
+from matplotlib import pyplot as plt
 import cv2
 import matplotlib
 matplotlib.use("Agg")
-from matplotlib import pyplot as plt
-import numpy as np
-import os
-import struct
-import tqdm
-import yaml
 
 with open("config.yaml") as f:
-    config=yaml.safe_load(f)
+    config = yaml.safe_load(f)
 
-file_path=config["param_dir"]
-file_list=os.listdir(file_path)
-file_list=[i for i in file_list if ".bin" in i]
-file_num=len(os.listdir(file_path))
+file_path = config["param_dir"]
+file_list = os.listdir(file_path)
+file_list = [i for i in file_list if ".bin" in i]
+file_num = len(os.listdir(file_path))
 
-param_size=int(config["param_size"])
-gene_num=int(config["gene_num"])
+param_size = int(config["param_size"])
+gene_num = int(config["gene_num"])
 
-#写真保存用のパスが存在するか確認してないようなら作成する
+# 写真保存用のパスが存在するか確認してないようなら作成する
 if not os.path.exists(config["img_dir"]):
     os.mkdir(config["img_dir"])
 
-#動画保存用のパスが存在するか確認してないようなら作成する
+# 動画保存用のパスが存在するか確認してないようなら作成する
 if not os.path.exists(config["mov_dir"]):
     os.mkdir(config["mov_dir"])
 
-#動画の設定
-mov_h=480
-mov_w=640
-fourcc=cv2.VideoWriter_fourcc("m","p","4","v")
-video=cv2.VideoWriter(os.path.join(config["mov_dir"],"result.mp4"),
-                                    fourcc,40.0,(mov_w,mov_h),True)
+# 動画の設定
+mov_h = 480
+mov_w = 640
+fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+video = cv2.VideoWriter(os.path.join(config["mov_dir"], "result.mp4"),
+                        fourcc, 40.0, (mov_w, mov_h), True)
 
-#世代を取得
-#ファイル名は必ずhoge_[世代].csvになっている必要がある
-L=[int(i.split("_")[1].split(".")[0]) for i in file_list]
+# 世代を取得
+# ファイル名は必ずhoge_[世代].csvになっている必要がある
+L = [int(i.split("_")[1].split(".")[0]) for i in file_list]
 L.sort()
 print(L)
 
 plt.figure()
 for j in tqdm.tqdm(range(len(L))):
-    i=L[j]
-    filename=os.path.join(file_path,"out_{}.bin".format(i))
-    if os.path.exists(os.path.join(config["img_dir"],"out_{}.png".format(i))):
-        #動画への出力
-        img=cv2.imread(os.path.join(config["img_dir"],"out_{}.png".format(i)))
+    i = L[j]
+    filename = os.path.join(file_path, "out_{}.bin".format(i))
+    if os.path.exists(os.path.join(config["img_dir"], "out_{}.png".format(i))):
+        # 動画への出力
+        img = cv2.imread(os.path.join(
+            config["img_dir"], "out_{}.png".format(i)))
         video.write(img)
         continue
-    
 
     # バイナリデータの読み込み
-    with open(filename,"rb") as f:
-        rawdata=f.read()
-    
-    data=[[struct.unpack_from("f",rawdata,4*(param_size*i+j))[0] for j in range(param_size)] for i in range(gene_num)]
-    data=np.array(data)
+    with open(filename, "rb") as f:
+        rawdata = f.read()
 
-    #ヒートマップの描画
+    data = [[struct.unpack_from("f", rawdata, 4*(param_size*i+j))[0]
+             for j in range(param_size)] for i in range(gene_num)]
+    data = np.array(data)
+
+    # ヒートマップの描画
     plt.clf()
-    plt.imshow(data,interpolation="nearest",vmin=-1,vmax=1,cmap="jet",aspect=config["aspect"])
+    plt.imshow(data, interpolation="nearest", vmin=-1,
+               vmax=1, cmap="jet", aspect=config["aspect"])
     plt.colorbar()
     plt.title("Generation: {}".format(i))
-    plt.savefig(os.path.join(config["img_dir"],"out_{}.png".format(i)))
+    plt.savefig(os.path.join(config["img_dir"], "out_{}.png".format(i)))
 
-    #動画への出力
-    img=cv2.imread(os.path.join(config["img_dir"],"out_{}.png".format(i)))
+    # 動画への出力
+    img = cv2.imread(os.path.join(config["img_dir"], "out_{}.png".format(i)))
     video.write(img)
 video.release()
