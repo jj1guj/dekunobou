16a8e97976a6679dfb2735110676484359402610
JJ1GUJ <jj1guj@jj1guj.net>
2023-09-07 20:48:14 +0900

    fix URL

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -361,5 +361,6 @@ function tweet(){
     }
     console.log(dataText);
     window.open("https://twitter.com/share?text="
-                +dataText+"&url=https://jj1guj.net/dekunobou-web/"+"&hashtags=dekunobou");
+                +dataText+"&url=https://dekunobou.jj1guj.net/"+"&hashtags=dekunobou");
+}
 }
