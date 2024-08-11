c181250ec473583b05560a192785153eac266fce
jj1guj <jj1guj@jj1guj.net>
2024-08-12 04:50:09 +0900

    use json format to pass the data

--- web/api/app.py
+++ web/api/app.py
@@ -11,15 +11,23 @@ CORS(api)
 # エンジンからの指し手を送る
 @api.route("/put", methods=["PUT"])
 def put():
-    board = request.form["board"]
-    turn = request.form["turn"]
+    body = request.json
+    board = body["board"]
+    turn = body["turn"]
 
     # バイナリに実行権限を与える
     subprocess.run(["chmod", "u+x", "web/api/dekunobou"])
 
     cmd = ["web/api/dekunobou", "--mode", "web", "-b", board, "-t", turn]
+
+    # 他にオプションが指定されていたら指定されたオプションを追加する
+    if "depth" in body:
+        cmd += ["--depth", body["depth"]]
+    if "perfect_search_depth" in body:
+        cmd += ["--perfect_search_depth", body["perfect_search_depth"]]
+
     cp = subprocess.run(cmd, encoding="utf-8", stdout=subprocess.PIPE)
-    return str(int(cp.stdout))
+    return jsonify({"move": str(int(cp.stdout))})
 
 
 @api.route("/")
@@ -29,4 +37,4 @@ def index():
 
 if __name__ == "__main__":
     api.run(host="0.0.0.0", port=int(
-        os.environ.get("PORT", 5000)), debug=False)
+        os.environ.get("PORT", 5000)), debug=True)
