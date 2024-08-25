6e6b5b4de3d4e8798d763fbd1ec948c5e2f1204f
jj1guj <jj1guj@jj1guj.net>
2024-08-25 22:28:18 +0900

    add option of issue #25

--- web/api/app.py
+++ web/api/app.py
@@ -14,17 +14,25 @@ def put():
     body = request.json
     board = body["board"]
     turn = body["turn"]
+    depth = body["depth"] if "depth" in body else None
+    perfect_search_depth = body["perfect_search_depth"] if "perfect_search_depth" in body else None
+
+    # ToDo リクエストのバリデーションを追加する
 
     # バイナリに実行権限を与える
     subprocess.run(["chmod", "u+x", "web/api/dekunobou"])
 
+    # 実行コマンドを構成する
+    turn = str(turn)
     cmd = ["web/api/dekunobou", "--mode", "web", "-b", board, "-t", turn]
 
     # 他にオプションが指定されていたら指定されたオプションを追加する
-    if "depth" in body:
-        cmd += ["--depth", body["depth"]]
-    if "perfect_search_depth" in body:
-        cmd += ["--perfect_search_depth", body["perfect_search_depth"]]
+    if depth is not None:
+        depth = str(depth)
+        cmd += ["--depth", depth]
+    if perfect_search_depth is not None:
+        perfect_search_depth = str(perfect_search_depth)
+        cmd += ["--perfect_search_depth", perfect_search_depth]
 
     cp = subprocess.run(cmd, encoding="utf-8", stdout=subprocess.PIPE)
     return jsonify({"move": str(int(cp.stdout))})
@@ -37,4 +45,4 @@ def index():
 
 if __name__ == "__main__":
     api.run(host="0.0.0.0", port=int(
-        os.environ.get("PORT", 5000)), debug=True)
+        os.environ.get("PORT", 5000)), debug=False)
