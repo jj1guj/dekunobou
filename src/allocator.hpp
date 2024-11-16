f8a0c7e4a2741546c3291aeff7c841a2cd51cf95
jj1guj <jj1guj@jj1guj.net>
2024-11-16 09:45:47 +0900

    enable to build on MacOS

--- src/allocator.hpp
+++ src/allocator.hpp
@@ -11,5 +11,12 @@ class MallocAllocator {
     return static_cast<T*>(std::malloc(n * sizeof(T)));
   }
 
+  // 等価比較演算子の定義
+  bool operator==(const MallocAllocator& other) const noexcept {
+    return true;  // 全てのインスタンスが等価であると仮定
+  }
+
+  bool operator!=(const MallocAllocator& other) const noexcept { return false; }
+
   void deallocate(T* p, std::size_t) noexcept { std::free(p); }
 };
